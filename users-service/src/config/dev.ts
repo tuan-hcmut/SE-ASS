38d262ca22e4680ee1c85522eab20dc0d6cb70d3
tuan <tuan01677239677@gmail.com>
2023-03-08 23:17:21 +0700

    push docker image from ECR to EKS

--- users-service/src/config/dev.ts
+++ users-service/src/config/dev.ts
@@ -1,6 +1,6 @@
 module.exports = {
-  googleClientID: "713214247686-dl5e5bmtqs819ph11ho5smn7q6esoeoq.apps.googleusercontent.com",
-  googleClientSecret: "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",
-  mongoURI: "mongodb+srv://tuanne123456789:za2uQMGLYu97KToO@cluster0.2uhwhwd.mongodb.net/test?retryWrites=true&w=majority",
-  cookieKey: "123123123",
+  googleClientID: process.env.GOOGLE_ID,
+  googleClientSecret: process.env.GOOGLE_SECRET,
+  mongoURI: process.env.MONGO_URL,
+  cookieKey: process.env.COOKIE_KEY,
 };
--- users-service/src/index.ts
+++ users-service/src/index.ts
@@ -4,6 +4,9 @@ import { app } from "./app";
 const connectDB = async () => {
   if (!process.env.JWT_KEY) throw new Error("JWT_KEY must be defined!!!");
   if (!process.env.MONGO_URL) throw new Error("MONGO_URL must be defined!!!");
+  if (!process.env.GOOGLE_ID) throw new Error("GOOGLE_ID must be defined!!!");
+  if (!process.env.GOOGLE_SECRET) throw new Error("GOOGLE_SECRET must be defined!!!");
+  if (!process.env.COOKIE_KEY) throw new Error("COOKIE_KEY must be defined!!!");
 
   try {
     await mongoose.connect(process.env.MONGO_URL, {
