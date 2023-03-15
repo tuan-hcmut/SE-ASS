46af6473140e6f6f677a7375974272683dc6d07f
tuan <tuan01677239677@gmail.com>
2023-03-15 22:14:42 +0700

    change

--- client-service/src/pages/SelectRole.tsx
+++ client-service/src/pages/SelectRole.tsx
@@ -16,7 +16,7 @@ const SelectRole: React.FC<Props> = ({ loading, setLoading }) => {
   return (
     <>
       <div className="sky-bg w-screen h-screen">
-        <div>hell</div>
+        <div>hello</div>
         <div className="sun"></div>
         <div className="mercury"></div>
         <div className="venus"></div>
